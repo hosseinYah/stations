aa449438ac84e2f669aeea292f52e727dac17f0e
Hossein <hossein@xeleva.com>
2024-04-26 12:17:22 -0600

    back-end api added

---
+++ app/api/stations/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+import { getStations } from "../../lib/data";
+
+// get all the stations
+export const GET = async (req:Request, res: Response) => {
+    console.log('GET-stations');
+
+    try {
+        const stations = getStations();
+        return  NextResponse.json({ message: 'OK', stations}, {status:200})
+
+    } catch (error) {
+        return NextResponse.json({message : 'Eroor', error}, {status: 500})
+    }
+
+}
---
+++ app/api/trips/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+import { getTrips, addTrip} from "../../lib/data";
+
+// get all the trips
+export const GET = async (req:Request, res: Response) => {
+    console.log('GET-trips');
+
+    try {
+        const trips = getTrips();
+        return  NextResponse.json({ message: 'OK', trips}, {status:200})
+
+    } catch (error) {
+        return NextResponse.json({message : 'Eroor', error}, {status: 500})
+    }
+}
+
+// add a trip to trip list
+export const POST = async (req:Request, res: Response) => {
+    console.log('GET statement working');
+
+    try {
+        const trip = await req.json();
+        addTrip(trip);
+        return  NextResponse.json({ message: 'OK', trip}, {status:200})
+
+    } catch (error) {
+        return NextResponse.json({message : 'Eroor', error}, {status: 500})
+    }
+
+}
